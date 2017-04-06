7cb623489ca7e6964ef8602aa71f6946299e32be
Hunter Loftis <hunter@hunterloftis.com>
2017-04-06 14:30:27 -0400

    fix time

--- test/time.test.js
+++ test/time.test.js
@@ -2,15 +2,9 @@ const a = require('..')
 const assert = require('chai').assert
 
 describe('time', () => {
-  it('waits for epoch milliseconds if a number is a provided', async () => {
-    const start = Date.now()
-    await a.time(start + 100)
-    assert.closeTo(Date.now() - start, 100, 10)
-  })
   it('waits until a date if a Date object is provided', async () => {
-    const now = new Date()
-    const start = now.getTime()
-    const end = new Date(now.getYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds() + 100)
+    const start = Date.now()
+    const end = new Date(start + 100)
     await a.time(end)
     assert.closeTo(Date.now() - start, 100, 10)
   })
