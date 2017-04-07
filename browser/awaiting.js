e06e1912dbb30b1517b1c5fb051f1306dc0ffbde
Hunter Loftis <hunter@hunterloftis.com>
2017-04-07 15:03:07 -0400

    built

--- browser/awaiting.js
+++ browser/awaiting.js
@@ -1,5 +1,30 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.awaiting = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (process){
+/**
+ * The async/await utility for browsers and Node.js.
+ *
+ * **`$ yarn add awaiting`**
+ *
+ * **`$ npm install awaiting --save`**
+ *
+ * **`<script src='awaiting.js'>`**
+ *
+ * (
+ * [github](https://github.com/hunterloftis/awaiting) -
+ * [npm](https://www.npmjs.com/package/awaiting) -
+ * [issues](https://github.com/hunterloftis/awaiting/issues) -
+ * [installation](https://github.com/hunterloftis/awaiting#installation) -
+ * [motivation](https://github.com/hunterloftis/awaiting#motivation)
+ * )
+ *
+ * [![Build Status](https://travis-ci.org/hunterloftis/awaiting.svg?branch=master)](https://travis-ci.org/hunterloftis/awaiting)
+ *
+ * @file
+ * @example
+ *
+ * const a = require('awaiting')
+ */
+
 module.exports = {
   delay,
   time,
--- test/browser/test.js
+++ test/browser/test.js
@@ -1,5 +1,30 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (process){
+/**
+ * The async/await utility for browsers and Node.js.
+ *
+ * **`$ yarn add awaiting`**
+ *
+ * **`$ npm install awaiting --save`**
+ *
+ * **`<script src='awaiting.js'>`**
+ *
+ * (
+ * [github](https://github.com/hunterloftis/awaiting) -
+ * [npm](https://www.npmjs.com/package/awaiting) -
+ * [issues](https://github.com/hunterloftis/awaiting/issues) -
+ * [installation](https://github.com/hunterloftis/awaiting#installation) -
+ * [motivation](https://github.com/hunterloftis/awaiting#motivation)
+ * )
+ *
+ * [![Build Status](https://travis-ci.org/hunterloftis/awaiting.svg?branch=master)](https://travis-ci.org/hunterloftis/awaiting)
+ *
+ * @file
+ * @example
+ *
+ * const a = require('awaiting')
+ */
+
 module.exports = {
   delay,
   time,
